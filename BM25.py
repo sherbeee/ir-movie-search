6edb560589e1f64bc83da4dfc08b1d819ae9eb3a
Gabriel <omnifarter@gmail.com>
2022-07-29 23:00:04 +0800

    finished BM25

--- BM25.py
+++ BM25.py
@@ -1,6 +1,19 @@
 
 import math
+import pickle
+import time
+import pandas as pd
 import numpy as np
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import string
+import ast
+
+from os.path import exists
+
+
+NUMBER_OF_RESULTS = 10
 
 
 class BM25Okapi():
@@ -8,6 +21,9 @@ class BM25Okapi():
         self.k1 = k1
         self.b = b
         self.calculate_corpus(corpus)
+
+    def set_title_mapping(self, titles):
+        self.titles = titles
 
     def calculate_corpus(self, corpus):
         self.corpus_size = len(corpus)
@@ -27,6 +43,8 @@ class BM25Okapi():
         self.average_doc_length = total_token_count / self.corpus_size
 
         self.idf = self.idf_(self.df, self.corpus_size)
+
+        print('done??')
 
     def tf_(self, doc):
         tf = {}
@@ -67,15 +85,67 @@ class BM25Okapi():
         score = np.zeros(self.corpus_size)
         doc_len = np.array(self.doc_len)
         for term in tokens:
-            if term not in self.tf.keys() or self.idf.keys():
+            if term not in self.idf.keys():
                 continue
-            term_freq = np.array([doc[term] for doc in self.tf])
+            term_freq = np.array(
+                [doc[term] if term in doc.keys() else 0 for doc in self.tf])
             score += (self.idf[term] * ((self.k1 + 1) * term_freq /
-                                        (self.k1 * ((1 - self.b) + self.b * (doc_len / self.avgdl)) + term_freq)))
+                                        (self.k1 * ((1 - self.b) + self.b * (doc_len / self.average_doc_length)) + term_freq)))
         return score
 
+    def query(self, query):
+        # tokenize
+        tokens = word_tokenize(query)
 
-# TODO: put in parsed_data.csv as corpus
-# TODO: add mapping of doc number to movie title
-# TODO: return ranking.
-BM25Okapi()
+        # stop words
+        stop_words = set(stopwords.words('english'))
+        tokens = [t for t in tokens if t.lower(
+        ) not in stop_words and t.lower() not in string.punctuation]
+
+        # stemming
+        ps = PorterStemmer()
+        tokens = [ps.stem(t) for t in tokens]
+
+        # get scores
+        results = self.get_scores(tokens)
+
+        ranked_titles = [{"title": self.titles[i], "score":score}
+                         for i, score in enumerate(results)]
+
+        ranked_titles.sort(
+            key=lambda x: x['score'], reverse=True)
+
+        return ranked_titles[:NUMBER_OF_RESULTS]
+
+
+file_exists = exists('BM25.pickle')
+
+if file_exists:
+    file = open("BM25.pickle", "rb")
+    model = pickle.load(file)
+else:
+    # load csv.
+    movie_data = pd.read_csv('parsed_data.csv', header=0)
+    raw_list = movie_data.values.tolist()
+
+    corpus = []
+    titles = []
+    for row in raw_list:
+        titles.append(row[1])
+        corpus.append(ast.literal_eval(row[2]))
+
+    model = BM25Okapi(corpus)
+    model.set_title_mapping(titles)
+
+    # save to pickle
+    file = open("BM25.pickle", "wb")
+    pickle.dump(model, file)
+
+
+print("model loaded.")
+while True:
+    query_input = input("Please enter a query.")
+    start_time = time.time()
+    results = model.query(query_input)
+    print("results - ", results)
+    print("time taken - ", (time.time() - start_time), 'seconds')
